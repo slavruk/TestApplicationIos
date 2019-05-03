878a8209ddc8e9e77c78fb77067b78be25e3fa2b
Stas Lavruk <stas.lavruk@icloud.com>
2019-05-03 21:51:56 +0300

    Creating project and architecture.

---
+++ TestApplicationIos/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApplicationIos
+//
+//  Created by Stas Lavruk on 03/05/2019.
+//  Copyright © 2019 Stas Lavruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestApplicationIos/Services/Network/AFNetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  AFNetworkManager.swift
+//  TestApplicationIos
+//
+//  Created by Stas Lavruk on 03/05/2019.
+//  Copyright © 2019 Stas Lavruk. All rights reserved.
+//
+
+import Foundation
